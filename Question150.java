aeeb070e48e4a556069a2b61230d68024680d959
KANISHKA AGRAWAL <156896843+Kanishkaagrawal006@users.noreply.github.com>
2025-05-19 18:33:06 +0530

    Added Question150.java

---
+++ Question150.java
@@ -0,0 +1,32 @@
+public class Question150 {
+    public int evalRPN(String[] tokens) {
+         Stack<Integer> stack = new Stack<Integer>();
+     for (int i = 0; i < tokens.length; i++) {
+         if(tokens[i].equals("+")){
+             int first = stack.pop();
+             int second = stack.pop();
+             stack.push(first + second);
+         }
+         else if (tokens[i].equals("-")){
+             int first = stack.pop();
+             int second = stack.pop();
+             stack.push(second -first);
+         }
+         else if (tokens[i].equals("*")){
+             int first = stack.pop();
+             int second = stack.pop();
+             stack.push(first * second);
+         }
+         else if (tokens[i].equals("/")){
+             int first = stack.pop();
+             int second = stack.pop();
+             stack.push(second/first);
+         }
+         else{
+             stack.push(Integer.parseInt(tokens[i]));
+         }
+         }
+     return stack.pop();
+        
+    }
+}
